f569a064b62d400339afd5c1d5cd8e8c27cc0a9b
Jaroslaw.Przybyl <jaroslaw_przybyl@rossmann.com.pl>
2025-04-24 15:48:12 +0200

    Users module init

---
+++ src/api/Ecommerce.Api/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapGet("/", () => "It's Ecommerce API!");
+
+await app.RunAsync();
